f7b96d4759768358b928088277e7010e55cfd05d
julianbedoya <lynxwork23@gmail.com>
2022-05-12 23:45:31 -0500

    commit Operaciones

---
+++ Clase01/Operaciones/javascript/main.js
@@ -0,0 +1,59 @@
+
+ // Aritmética Básica
+ // suma 
+let a = 2;
+let b = 4;
+let resultadoSUM = a+b;
+console.log("SUMA", resultado);
+
+ // resta 
+ let c = 20;
+ let d = 40;
+ let resultadoREST = c-d;
+ console.log("RESTA", resultado);
+  // Multiplicacion 
+let e = 52;
+let f = 34;
+let resultadoMUL = e*f;
+console.log("MULTIPLICACION", resultado);
+ // Division 
+ let g = 29;
+ let h = 84;
+ let resultadoDIV = g/h;
+ console.log("DIVISION", resultado);
+ //modulo
+ let x = 10;
+ let y = 20; 
+ let resultadoMOD = x%y;
+ console.log("MOD", resultadoMOD);
+
+
+ // Operaciones con Strings 
+ // concatenación
+ let nombre = "Edgar";
+ let apellido = "Gamboa";
+ let nombreCopmleto = nombre+" "+apellido;
+ console.log("CONCAT", nombreCopmleto);
+
+ 
+ // Casteo de Datos
+ let nombre2 = "Lucas";
+ let apellido2 = 10;
+ let nombreRaro = nombre2+apellido2;
+ console.log("NOMBRE RARO", nombreRaro);
+
+ let num1 = "10";
+ let num2 = "10";
+ let res = num1+num2;
+ console.log("SUMA RARA", res);
+
+
+ let num3 = Number("10"); // 10
+ let num4 = Number("10"); // 10 
+ let res2 = num3-num4;
+ console.log("RESTA RARA", res2);
+
+ let num5 = Number("HOLA"); // 10
+ let num6 = Number("10"); // 10 
+ let res3 = num5-num6;
+ console.log("RESTA RARA 3", res3);
