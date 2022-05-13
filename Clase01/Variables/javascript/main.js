594702759cfa0bf99506ce05636a93d5fb6f69ef
julianbedoya <lynxwork23@gmail.com>
2022-05-12 22:54:22 -0500

    commit Variables

---
+++ Clase01/Variables/javascript/main.js
@@ -0,0 +1,34 @@
+//var
+var empleado = "juanito perez"
+console.log(empleado);
+
+var empleado = "JUANITO LOPEZ"
+console.log(empleado);
+
+//let
+let producto = "azucar"
+console.log(producto);
+producto = "Manzana";
+console.log(producto);
+
+let productoDesayuno = "leche" //Camel Case
+
+
+/* 
+let producto = "OTRO PRODUCTO";
+console.log(producto);
+ERROR SI SE REDECLARA CON EL MISMO NOMBRE
+*/
+
+
+
+//const
+const carrito = "Carrito de Mercado"
+console.log(carrito);
+
+/*
+carrito = "OTRO CARRITO DE MERCADO";
+console.log(carrito); ERROR */
+
+
+
