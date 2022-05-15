e14341589c55fa2c9c7857b6fcf704ddc4ecac0e
julianbedoya <lynxwork23@gmail.com>
2022-05-14 23:57:11 -0500

    commit Clase02Condicionales IF / ELSE IF / ELSE

---
+++ Clase02Condicionales/IF_ELSEIF_ELSE/javascript/index.js
@@ -0,0 +1,39 @@
+const datoUsuario = prompt("Ingrese una Saludo:")
+
+  if(datoUsuario==="SALUDAR")
+  {
+    console.log("HOLA!")
+  }
+
+  // IF / ELSE IF
+  if(datoUsuario==="SALUDAR")
+  {
+    console.log("HOLA!")
+  }
+  else if(datoUsuario==="DESPEDIRSE")
+  {
+    console.log("Adios!")
+  }
+  else if(datoUsuario==="BIENVENIDA")
+  {
+    console.log("Bienvenido!")
+  }
+
+  // IF / ELSE IF / ELSE
+
+  if(datoUsuario==="SALUDAR")
+  {
+    console.log("HOLA!")
+  }
+  else if(datoUsuario==="DESPEDIRSE")
+  {
+    console.log("Adios!")
+  }
+  else if(datoUsuario==="BIENVENIDA")
+  {
+    console.log("Bienvenido!")
+  }
+  else
+  {
+    console.log("Opcion Invalida!!!!")
+  }
