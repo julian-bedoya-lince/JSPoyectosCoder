0d9a100ed36f351a3a6e20f026835945fa6c3455
julianbedoya <lynxwork23@gmail.com>
2022-05-15 02:15:05 -0500

    commit Clase03 Ciclos - WHILE

---
+++ Clase03Ciclos/While/javascript/index.js
@@ -0,0 +1,52 @@
+let i=1;
+
+  while(i<=10)
+  {
+      console.log(i);
+      i++;
+  }
+
+// Solo pares  V1
+console.log("Solo Pares V1")
+let j=0;
+  while(j<=10)
+  {
+      console.log(j);
+      j+=2;
+  }
+// Solo pares V2  
+  console.log("Solo Pares V2")
+  
+  let k=0;
+
+  while(k<=10)
+  {
+      if(k%2===0)
+      {
+         console.log(k);
+      }
+      k++;
+  }
+
+// Solo Impares V1
+console.log("Solo Impares V1")  
+let h=1;
+  while(h<=10)
+  {
+    if(h%2!==0)
+    {
+        console.log(h);
+    }
+    h++;
+  }
+
+  // Solo Impares V2
+
+  console.log("Solo Impares V2")
+let c=1;
+while(c<=10)
+{
+   
+    console.log(c);
+    c+=2;
+}
