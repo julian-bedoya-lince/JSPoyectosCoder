772618544e8a4bb9a2096f67823d37071ce2b052
julianbedoya <lynxwork23@gmail.com>
2022-05-25 16:26:37 -0500

    Commit Clase 05 Metodos En Objetos

---
+++ Clase05Objetos/MetodosEnObjetos/javascript/main.js
@@ -0,0 +1,26 @@
+function Producto(id, nombre, precio) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = precio;
+
+  this.obtenerIva = function () {
+    const iva = this.precio * 0.2;
+    return iva;
+  };
+
+  this.obtenerPrecioFinal = () => this.precio * this.obtenerIva();
+}
+
+const producto = new Producto(0, "carne", 200);
+console.log("IVA:", producto.obtenerIva());
+console.log("PRECIOFINAL:", producto.obtenerPrecioFinal());
+
+// Operador IN
+
+console.log("Precio", "precio" in producto);
+
+// recorrer propiedades
+
+for (let prop in producto) {
+  console.log(prop);
+}
