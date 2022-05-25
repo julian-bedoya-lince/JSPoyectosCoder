fcd5bd68eb70bc9c95bf72efd29aa03c516b0c48
julianbedoya <lynxwork23@gmail.com>
2022-05-25 17:30:36 -0500

    Commit Clase 06 Arrays Acceso

---
+++ Clase06Arrays/Acceso/javascript/main.js
@@ -0,0 +1,11 @@
+const misNumeros = [10, 9, 8, 7, 6, 6, 100, 200, 300];
+// Posicion o Indice 0,1,2,3,4,5...
+
+console.log(misNumeros[0]);
+console.log(misNumeros[7]);
+console.log(misNumeros[100]);
+
+// recorrer un array de la casilla incial, a la 5
+for (let i = 0; i <= 5; i++) {
+  console.log(misNumeros[i]);
+}
