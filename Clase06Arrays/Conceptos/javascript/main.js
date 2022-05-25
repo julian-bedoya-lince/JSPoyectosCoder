75ac5e8e03055b14ea7d63cb27751a2b20ed429a
julianbedoya <lynxwork23@gmail.com>
2022-05-25 17:23:01 -0500

    Commit Clase 06 Arrays Conceptos

---
+++ Clase06Arrays/Conceptos/javascript/main.js
@@ -0,0 +1,25 @@
+//numeros
+const misNumeros = [100, 24, 49, 55, 29];
+console.log("NUMEROS", misNumeros);
+
+//cadenas
+const misPalabras = ["hola", "julian", "como", "estas", "?"];
+console.log("FRASE", misPalabras);
+
+//booleanos
+const misBooleanos = [true, false, true, false, false];
+console.log("BOOLEANOS", misBooleanos);
+
+//mixto
+const mixto = [
+  true,
+  "hola",
+  1,
+  -2,
+  { nombre: "Julian", id },
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  () => {
+    console.log("HOLA!");
+  },
+];
+console.log("MIXTO", mixto);
