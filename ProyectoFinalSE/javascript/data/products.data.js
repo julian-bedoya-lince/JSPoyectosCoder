555092a9ef618a0ca32d205e274bb3abc06c5d46
julianbedoya <lynxwork23@gmail.com>
2022-07-17 23:09:05 -0500

    Se sube el Proyecto Final 3ra Entrega

---
+++ ProyectoFinalSE/javascript/data/products.data.js
@@ -0,0 +1,139 @@
+const products = [
+  {
+    id: 53021,
+    title: "P Virgen Milagrosa Mini 16.5 cms Extensor Cruz",
+    price: 90000,
+    description: "P Virgen Milagrosa Mini 16.5 cms Extensor Cruz",
+    category: "PULSERAS",
+    image:
+      "https://belatrizcolombia.com/app/public/img_productos/53021/PERFIL.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 34004,
+    title: "A Flor Circonias",
+    price: 55000,
+    description: "A Flor Circonias",
+    category: "ARETES",
+    image:
+      "https://belatrizcolombia.com/app/public/img_productos/34004/PERFIL.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 101,
+    title: "Dije Cruz",
+    price: 85000,
+    description: " dorado",
+    category: "DIJES",
+    image: "/img/dige1.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 102,
+    title: "Dije Cruz",
+    price: 135000,
+    description: " dorado",
+    category: "DIJES",
+    image: "/img/dige2.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 103,
+    title: "Dije Cruz",
+    price: 123000,
+    description: " dorado",
+    category: "DIJES",
+    image: "/img/dige3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 104,
+    title: "Dije Cruz",
+    price: 135000,
+    description: " dorado",
+    category: "DIJES",
+    image: "/img/dige4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 201,
+    title: "cadenas Cordon Cuadrado",
+    price: 145000,
+    description: "Cadena ",
+    category: "CADENA",
+    image: "/img/cadena1.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 202,
+    title: "cadenas Espiga",
+    price: 167000,
+    description: "Cadena ",
+    category: "CADENA",
+    image: "/img/cadena5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 203,
+    title: "cadenas Aros",
+    price: 175000,
+    description: "Cadena ",
+    category: "CADENA",
+    image: "/img/cadena3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 204,
+    title: "cadenas Espiga Gruesa",
+    price: 195000,
+    description: "Cadena ",
+    category: "CADENA",
+    image: "/img/cadena4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 301,
+    title: "Aretes",
+    price: 95000,
+    description: "Aretes",
+    category: "ARETES",
+    image: "/img/aretes1.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 302,
+    title: "Aretes Petalos",
+    price: 100000,
+    description: "Aretes ",
+    category: "ARETES",
+    image: "/img/aretes5.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 303,
+    title: "Aretes Balines",
+    price: 97000,
+    description: "Aretes ",
+    category: "ARETES",
+    image: "/img/aretes3.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+
+  {
+    id: 304,
+    title: "Aretes Huella de Perro",
+    price: 75000,
+    description: "Aretes ",
+    category: "ARETES",
+    image: "/img/aretes4.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+];
